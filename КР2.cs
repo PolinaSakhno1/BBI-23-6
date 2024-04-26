2f4d24bea45bf5d9a4a2e9c133851980723e5202
PolinaSakhno1 <146880122+PolinaSakhno1@users.noreply.github.com>
2024-04-26 15:39:36 +0300

    Add files via upload

---
+++ КР2.cs
@@ -0,0 +1,58 @@
+﻿//Задача 1 
+using System.Text;
+
+StringBuilder result = new StringBuilder();
+
+foreach (string word in words)
+{
+    char[] letr = word.ToCharArray();
+    Array.Reverse(letr);
+    result.Append(new string(letr) + " ");
+}
+{ 
+return result.ToString().Trim();
+}
+
+static void Main()
+{
+    Console.WriteLine("Введите текст:");
+    string input = Console.ReadLine();
+
+    string output = reverr(input);
+
+    Console.WriteLine("перевёртыш: ");
+    Console.WriteLine(output);
+}
+
+
+//Задача 2
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите слово:");
+        string word = Console.ReadLine();
+
+        int Count1 = Count(word);
+        Console.WriteLine($"Количество букв: {Count1}");
+    }
+
+    static int Count(string word)
+    {
+        HashSet<char> un_letr = new HashSet<char>();
+
+        foreach (char letter in word)
+        {
+            if (!un_letr.Contains(letter))
+            {
+                un_letr.Add(letter);
+            }
+        }
+
+        return un_letr.Count;
+    }
+}
