adf618d96cb8292835a3675b4aa354da8f355d73
PolinaSakhno1 <146880122+PolinaSakhno1@users.noreply.github.com>
2024-04-30 18:59:00 +0300

    Add files via upload

---
+++ lab7_lv2_n4.cs
@@ -0,0 +1,166 @@
+﻿//2 (2,5)
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public struct Sportsman
+{
+    private string name;
+    private int[,] golos_jumps;
+    public int[,] Golosa { get { return golos_jumps; } }
+    public string Name { get { return name; } }
+    public Sportsman(string name, int[,] jumps_)
+    {
+        this.name = name;
+        golos_jumps = jumps_;
+    }
+
+}
+
+public abstract class JumpWater
+{
+    protected string name;
+    protected string dist;
+    protected Sportsman[] sportsmen;
+    protected double[] indexx;
+
+    public JumpWater(Sportsman[] sportsmen, double[] indexx)
+    {
+        name = "Прыжки в воду";
+        this.sportsmen = sportsmen;
+        this.indexx = indexx;
+    }
+    protected double RezultForPerson(int[,] list)
+    {
+        for (int number = 0; number < 4; number++)
+        {
+            for (int i = 1; i < 7; i++)
+            {
+                int k = list[number, i];
+                int j = i - 1;
+
+                while (j >= 0 && list[number, j] < k)
+                {
+                    list[number, j + 1] = list[number, j];
+                    j--;
+                }
+                list[number, j + 1] = k;
+            }
+        }
+        int[] res_jumps = new int[4] { 0, 0, 0, 0 };
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 1; j < 6; j++)
+            {
+                res_jumps[i] += list[i, j];
+            }
+        }
+        double s = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            s += res_jumps[i] * indexx[i];
+        }
+        return s;
+    }
+    protected Sportsman[] Sort(Sportsman[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            for (int j = i; j < list.Length; j++)
+                if (RezultForPerson(list[i].Golosa) < RezultForPerson(list[j].Golosa))
+                {
+                    Sportsman person_now = list[j]; // меняем
+                    list[j] = list[i]; // местами
+                    list[i] = person_now; // игроков
+                }
+        }
+        return list;
+    }
+
+    public void Sorevnovan()
+    {
+        Sportsman[] list_sort = Sort(sportsmen);
+        Print_table(list_sort);
+    }
+
+    protected void Print_table(Sportsman[] sort_list)
+    {
+        Console.WriteLine(name);
+        for (int i = 0; i < sort_list.Length; i++)
+        {
+            Console.Write($"{i + 1} место: ");
+            Console.WriteLine($"имя: {sort_list[i].Name} результат: {RezultForPerson(sort_list[i].Golosa)}");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Jump3 : JumpWater
+{
+    public Jump3(Sportsman[] sportsmen, double[] indexx) : base(sportsmen, indexx)
+    {
+        name = "Прыжки в воду c 3 м";
+    }
+}
+
+class Jump5 : JumpWater
+{
+    public Jump5(Sportsman[] sportsmen, double[] indexx) : base(sportsmen, indexx)
+    {
+        name = "Прыжки в воду c 5 м";
+    }
+}
+
+
+public class Program
+{
+    static double[] FillIndexx()
+    {
+        double[] indexx = new double[4];
+        for (int i = 0; i < 4; i++)
+        {
+            indexx[i] = double.Parse(Console.ReadLine());
+        }
+        return indexx;
+    }
+
+    static Sportsman FillInfo()
+    {
+        string name = Console.ReadLine();
+        int[,] golosa = new int[4, 7];
+        for (int j = 0; j < 4; j++) //для каждого прыжка
+        {
+            for (int s = 0; s < 7; s++)
+            {
+                int golos = int.Parse(Console.ReadLine());
+                golosa[j, s] = golos;
+            }
+        }
+        return new Sportsman(name, golosa);
+    }
+    public static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        Sportsman[] peoplelist = new Sportsman[n];
+        double[] indexx = FillIndexx();
+        for (int i = 0; i < n; i++) //каждый спортсмен
+        {
+            peoplelist[i] = FillInfo();
+        }
+        Jump3 Sorev = new Jump3(peoplelist, indexx);
+        Sorev.Sorevnovan();
+        Console.WriteLine("!");
+
+        int n2 = int.Parse(Console.ReadLine());
+        Sportsman[] peoplelist2 = new Sportsman[n2];
+        double[] indexx2 = FillIndexx();
+        for (int i = 0; i < n2; i++)
+        {
+            peoplelist2[i] = FillInfo();
+        }
+        Jump5 Sorev2 = new Jump5(peoplelist2, indexx2);
+        Sorev2.Sorevnovan();
+        Console.ReadKey();
+    }
+}
---
+++ lab7_lv3_n5.cs
@@ -0,0 +1,156 @@
+﻿//3
+using System;
+class Program
+{
+    class Team
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        private int scored;
+        private int missed;
+        private int points;
+        public Team(string name)
+        {
+            this.name = name;
+            points = 0;
+            scored = 0;
+            missed = 0;
+        }
+        public void Win(int score1, int score2)
+        {
+            scored += Math.Max(score1, score2);
+            missed += Math.Min(score1, score2);
+            points += 3;
+        }
+        public void Lose(int score1, int score2)
+        {
+            scored += Math.Min(score1, score2);
+            missed += Math.Max(score1, score2);
+        }
+        public void Draw(int score)
+        {
+            scored += score;
+            missed += score;
+            points += 1;
+        }
+        public int Getdiff()
+        {
+            return scored - missed;
+        }
+        public int Getpoints()
+        {
+            return points;
+        }
+        public void info(int place)
+        {
+            Console.WriteLine($"{place}\t{name}\t{points}");
+        }
+    }
+    class WomenTeam : Team
+    {
+        public WomenTeam(string name) : base(name + " женская")
+        {
+        }
+    }
+    class MenTeam : Team
+    {
+        public MenTeam(string name) : base(name + " мужская")
+        {
+        }
+    }
+
+    static void calculatepoints(Team team1, Team team2, int score1, int score2)
+    {
+        if(score1 > score2)
+        {
+            team1.Win(score1, score2);
+            team2.Lose(score1, score2);
+        }
+        else if (score1 < score2)
+        {
+            team2.Win(score1, score2);
+            team1.Lose(score1, score2);
+        }
+        else
+        {
+            team1.Draw(score1);
+            team2.Draw(score1);
+        }
+    }
+    static void Tournament(Team[] teams)
+    {
+        Random random = new Random();
+        for (int i = 0; i < teams.Length; i++)
+        {
+            for(int j = i + 1; j < teams.Length; j++)
+            {
+                int score1 = random.Next(0, 10);
+                int score2 = random.Next(0, 10);
+                Console.WriteLine($"{teams[i].Name} - {teams[j].Name} {score1}:{score2}");
+                calculatepoints(teams[i], teams[j], score1, score2);
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Team[] womenteams = new Team[4]
+        {
+            new WomenTeam("1"),
+            new WomenTeam("2"),
+            new WomenTeam("3"),
+            new WomenTeam("4")
+        };
+        Team[] menteams = new Team[4]
+        {
+            new MenTeam("1"),
+            new MenTeam("2"),
+            new MenTeam("3"),
+            new MenTeam("4")
+        };
+        Tournament(menteams);
+        Console.WriteLine();
+        Tournament(womenteams);
+        Console.WriteLine();
+        Team[] teams = new Team[menteams.Length + womenteams.Length];
+        for(int i = 0, j = 0; i < womenteams.Length; i++)
+        {
+            teams[j] = menteams[i];
+            j++;
+            teams[j] = womenteams[i];
+            j++;
+        }
+        sort(teams);
+        res(teams);
+        Console.ReadKey();
+    }
+
+
+    private static void sort(Team[] teams)
+    {
+        for (int i = 0; i < teams.Length; i++)
+        {
+            for (int j = 0; j < teams.Length - 1; j++)
+            {
+                if (teams[j].Getpoints() < teams[j + 1].Getpoints() || ((teams[j].Getpoints() == teams[j + 1].Getpoints()) && (teams[j].Getdiff() < teams[j + 1].Getdiff())))
+                {
+                    Team t = teams[j];
+                    teams[j] = teams[j + 1];
+                    teams[j + 1] = t;
+                }
+            }
+        }
+    }
+    private static void res(Team[] teams)
+    {
+
+        Console.WriteLine("Результирующая таблица:");
+        Console.WriteLine("Место\tКоманда\tПол\tОчки");
+        for (int i = 0; i < teams.Length; i++)
+        {
+            teams[i].info(i + 1);
+        }
+    }
+}
